1eaeb222a1a48bdd7b97f3c58e4841d729c7a277
TearPelato <165576102+TearPelato@users.noreply.github.com>
2025-10-11 14:10:56 +0200

    Fixed Crash when Sitting on Sittable Blocks(Patk Bench, Chair etc...)

--- src/main/java/net/tier1234/better_deco/entity/custom/ChairEntity.java
+++ src/main/java/net/tier1234/better_deco/entity/custom/ChairEntity.java
@@ -1,28 +1,15 @@
 package net.tier1234.better_deco.entity.custom;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MobSpawnType;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.BlockHitResult;
-import net.tier1234.better_deco.entity.ModEntities;
-
-import java.util.List;
 
 public class ChairEntity extends Entity {
     public ChairEntity(EntityType<?> entityType, Level level) {
         super(entityType, level);
     }
-
-
 
     @Override
     protected void defineSynchedData(SynchedEntityData.Builder builder) {
