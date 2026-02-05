2d48fc9b7fdc56f36d41ab4de1b3571998830765
TearPelato <165576102+TearPelato@users.noreply.github.com>
2026-02-05 22:23:07 +0100

    CreativeTab respawn

--- src/main/java/net/tier1234/better_deco/creative_tabs/BundledTabs.java
+++ src/main/java/net/tier1234/better_deco/creative_tabs/BundledTabs.java
@@ -2,13 +2,16 @@ package net.tier1234.better_deco.creative_tabs;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
+import net.neoforged.neoforge.server.ServerLifecycleHooks;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.BiConsumer;
 
 /**
@@ -16,44 +19,32 @@ import java.util.function.BiConsumer;
  * @author BlackGear
  */
 public class BundledTabs {
-    private final Component tooltip;
+    public final Component tooltip;
     private final ItemStack icon;
     private final List<ItemStack> displayItems;
-    private final @Nullable BiConsumer<HolderLookup.Provider, Output> populationLogic;
-    private boolean populated;
     private @Nullable BundledTabSelector.Tab tab;
-    private boolean selected;
+    private boolean selected = false;
 
-    private BundledTabs(
-            Component tooltip,
-            ItemStack icon,
-            List<ItemStack> staticItems,
-            @Nullable BiConsumer<HolderLookup.Provider, Output> populationLogic
-    ) {
+    private BundledTabs(Component tooltip, ItemStack icon, List<ItemStack> displayItems) {
         this.tooltip = tooltip;
         this.icon = icon;
-        this.displayItems = staticItems;
-        this.populationLogic = populationLogic;
+        this.displayItems = displayItems;
     }
 
     public static Builder builder() {
         return new Builder();
     }
 
-    public Component getTooltip() {
-        return this.tooltip;
-    }
-
     public ItemStack getIcon() {
         return this.icon;
     }
 
-    public List<ItemStack> getDisplayItems() {
-        return Collections.unmodifiableList(this.displayItems);
-    }
-
     public boolean contains(ItemStack stack) {
         return this.displayItems.contains(stack);
+    }
+
+    public List<ItemStack> getDisplayItems() {
+        return this.displayItems;
     }
 
     public void select() {
@@ -73,32 +64,21 @@ public class BundledTabs {
     }
 
     public void setVisible(boolean visible) {
-        if (this.tab != null) this.tab.visible = visible;
+        if (this.tab != null) {
+            this.tab.visible = visible;
+        }
     }
 
     public void setY(int y) {
-        if (this.tab != null) this.tab.setY(y);
-    }
-
-    public void populate(HolderLookup.Provider provider) {
-        if (this.populated || this.populationLogic == null) return;
-        this.populationLogic.accept(provider, new Output() {
-            @Override
-            public void accept(ItemLike item) {
-                displayItems.add(new ItemStack(item));
-            }
-            @Override
-            public void accept(ItemStack stack) {
-                displayItems.add(stack);
-            }
-        });
-        this.populated = true;
+        if (this.tab != null) {
+            this.tab.setY(y);
+        }
     }
 
     public static class Builder {
         private Component title;
         private ItemStack icon;
-        private BiConsumer<HolderLookup.Provider, Output> populationLogic;
+        private final List<ItemStack> displayItems = new ArrayList<>();
 
         public Builder title(Component title) {
             this.title = title;
@@ -110,20 +90,37 @@ public class BundledTabs {
             return this;
         }
 
-        public Builder displayItems(BiConsumer<HolderLookup.Provider, Output> logic) {
-            this.populationLogic = logic;
+        public Builder displayItems(BiConsumer<HolderLookup.Provider, Output> consumer) {
+            Optional<MinecraftServer> server =Optional.ofNullable(ServerLifecycleHooks.getCurrentServer());
+            assert server.isPresent();
+            HolderLookup.Provider provider = server.get().registryAccess();
+
+            Output output = new Output() {
+                @Override
+                public void accept(ItemLike item) {
+                    displayItems.add(new ItemStack(item));
+                }
+
+                @Override
+                public void accept(ItemStack stack) {
+                    displayItems.add(stack);
+                }
+            };
+
+            consumer.accept(provider, output);
             return this;
         }
 
         public BundledTabs build() {
             if (this.title == null) this.title = Component.empty();
             if (this.icon == null) this.icon = ItemStack.EMPTY;
-            return new BundledTabs(this.title, this.icon, new ArrayList<>(), this.populationLogic);
+            return new BundledTabs(this.title, this.icon, new ArrayList<>(this.displayItems));
         }
     }
 
     public interface Output {
         void accept(ItemLike item);
+
         void accept(ItemStack stack);
     }
 }
