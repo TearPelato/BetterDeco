c9bd1a2a2a800e3ae07c0f534184c1c4ea86af3b
TearPelato <165576102+TearPelato@users.noreply.github.com>
2026-02-11 20:35:07 +0100

    Fixed Microwave Recipe
    IDK what I touched months ago but now the recipe are back

--- src/main/java/net/tier1234/better_deco/recipe/MicrowaveRecipe.java
+++ src/main/java/net/tier1234/better_deco/recipe/MicrowaveRecipe.java
@@ -3,6 +3,7 @@ package net.tier1234.better_deco.recipe;
 
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.client.RecipeBookCategories;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.NonNullList;
 import net.minecraft.network.RegistryFriendlyByteBuf;
@@ -15,16 +16,10 @@ import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
 
-public record MicrowaveRecipe(ResourceLocation id, Ingredient inputItem, ItemStack output)
-        implements Recipe<MicrowaveRecipeInput> {
+public record MicrowaveRecipe(Ingredient inputItem, ItemStack output) implements Recipe<MicrowaveRecipeInput> {
     // inputItem & output ==> Read From JSON File!
     // MicrowaveRecipeInput --> INVENTORY of the Block Entity
 
-    public ResourceLocation getId() {
-        return id;
-    }
-
-    @Override
     public NonNullList<Ingredient> getIngredients() {
         NonNullList<Ingredient> list = NonNullList.create();
         list.add(inputItem);
@@ -32,15 +27,16 @@ public record MicrowaveRecipe(ResourceLocation id, Ingredient inputItem, ItemSta
     }
 
     @Override
-    public boolean matches(MicrowaveRecipeInput microwaveRecipeInput, Level level) {
+    public boolean matches(MicrowaveRecipeInput MicrowaveRecipeInput, Level level) {
         if (level.isClientSide()) {
             return false;
         }
-        return inputItem.test(microwaveRecipeInput.getItem(0));
+
+        return inputItem.test(MicrowaveRecipeInput.getItem(0));
     }
 
     @Override
-    public ItemStack assemble(MicrowaveRecipeInput microwaveRecipeInput, HolderLookup.Provider provider) {
+    public ItemStack assemble(MicrowaveRecipeInput MicrowaveRecipeInput, HolderLookup.Provider provider) {
         return output.copy();
     }
 
@@ -55,29 +51,26 @@ public record MicrowaveRecipe(ResourceLocation id, Ingredient inputItem, ItemSta
     }
 
     @Override
-    public RecipeSerializer<?> getSerializer() {
+    public RecipeSerializer<? extends Recipe<MicrowaveRecipeInput>> getSerializer() {
         return ModRecipes.MICROWAVE_SERIALIZER.get();
     }
 
     @Override
-    public RecipeType<?> getType() {
+    public RecipeType<? extends Recipe<MicrowaveRecipeInput>> getType() {
         return ModRecipes.MICROWAVE_TYPE.get();
     }
 
     public static class Serializer implements RecipeSerializer<MicrowaveRecipe> {
         public static final MapCodec<MicrowaveRecipe> CODEC = RecordCodecBuilder.mapCodec(inst -> inst.group(
-                ResourceLocation.CODEC.fieldOf("id").forGetter(MicrowaveRecipe::getId),
-                Ingredient.CODEC_NONEMPTY.fieldOf("ingredient").forGetter(MicrowaveRecipe::inputItem),
+                Ingredient.CODEC.fieldOf("ingredient").forGetter(MicrowaveRecipe::inputItem),
                 ItemStack.CODEC.fieldOf("result").forGetter(MicrowaveRecipe::output)
         ).apply(inst, MicrowaveRecipe::new));
 
         public static final StreamCodec<RegistryFriendlyByteBuf, MicrowaveRecipe> STREAM_CODEC =
                 StreamCodec.composite(
-                        ResourceLocation.STREAM_CODEC, MicrowaveRecipe::getId,
                         Ingredient.CONTENTS_STREAM_CODEC, MicrowaveRecipe::inputItem,
                         ItemStack.STREAM_CODEC, MicrowaveRecipe::output,
-                        MicrowaveRecipe::new
-                );
+                        MicrowaveRecipe::new);
 
         @Override
         public MapCodec<MicrowaveRecipe> codec() {
