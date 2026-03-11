d227b8cc75e649576c3a89d5c57bb2f3c2947af1
TearPelato <165576102+TearPelato@users.noreply.github.com>
2026-03-11 22:13:48 +0100

    Splitted client from main class

--- src/main/java/net/tier1234/better_deco/BetterDeco.java
+++ src/main/java/net/tier1234/better_deco/BetterDeco.java
@@ -38,7 +38,7 @@ public class BetterDeco {
     public static final Logger LOGGER = LogUtils.getLogger();
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public BetterDeco() {
+    public BetterDeco(  ) {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         // Register ourselves for server and other game events we are interested in.
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
@@ -52,7 +52,6 @@ public class BetterDeco {
         ModCreativeTabs.register(modEventBus);
         ModMenuTypes.register(modEventBus);
         ModRecipes.register(modEventBus);
-
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -88,32 +87,9 @@ public class BetterDeco {
 
     }
 
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+  /*  @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
 
-            EntityRenderers.register(ModEntities.CHAIR_ENTITY.get(), ChairRenderer::new);
-
-            MenuScreens.register(ModMenuTypes.CRATE_MENU.get(), CrateScreen::new);
-            MenuScreens.register(ModMenuTypes.FREEZER_MENU.get(), FreezerScreen::new);
-            MenuScreens.register(ModMenuTypes.SHELF_MENU.get(), ShelfScreen::new);
-            MenuScreens.register(ModMenuTypes.PEDESTAL_MENU.get(), PedestalScreen::new);
-            MenuScreens.register(ModMenuTypes.TECQUE_MENU.get(),TecqueScreen::new);
-            MenuScreens.register(ModMenuTypes.SINK_MENU.get(), SinkScreen::new);
-            MenuScreens.register(ModMenuTypes.MICROWAVE_MENU.get(), MicrowaveScreen::new);
-            MenuScreens.register(ModMenuTypes.OVEN_MENU.get(), OvenScreen::new);
-
-
-        }
-        @SubscribeEvent
-        public static void registerBER(EntityRenderersEvent.RegisterRenderers event) {
-            event.registerBlockEntityRenderer(ModBlockEntities.PEDESTAL_BE.get(), PedestalBlockEntityRenderer::new);
-            event.registerBlockEntityRenderer(ModBlockEntities.GLASS_TECQUE.get(), TecqueBlockEntityRenderer::new);
-            event.registerBlockEntityRenderer(ModBlockEntities.SHELF_BE.get(), ShelfBlockEntityRenderer::new);
-            event.registerBlockEntityRenderer(ModBlockEntities.DIGITAL_CLOCK.get(), DigitalClockBlockEntityRenderer::new);
-            event.registerBlockEntityRenderer(ModBlockEntities.KITCHEN_SINK.get(), KitchenSinkBlockEntityRenderer::new);
-        }
-    }
+    }*/
 
 }
--- src/main/java/net/tier1234/better_deco/BetterDecoClient.java
+++ src/main/java/net/tier1234/better_deco/BetterDecoClient.java
@@ -5,7 +5,6 @@ import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.tier1234.better_deco.block.entity.ModBlockEntities;
@@ -15,33 +14,31 @@ import net.tier1234.better_deco.entity.client.ChairRenderer;
 import net.tier1234.better_deco.screen.ModMenuTypes;
 import net.tier1234.better_deco.screen.custom.*;
 
-//@Mod.EventBusSubscriber(modid = BetterDeco.MOD_ID, value = Dist.CLIENT)
+@Mod.EventBusSubscriber(modid = BetterDeco.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD,value = Dist.CLIENT)
 public class BetterDecoClient {
-   /* public BetterDecoClient(ModContainer container) {
-        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
-    }
     @SubscribeEvent
-    static void onClientSetup(FMLClientSetupEvent event) {
+    public static void onClientSetup(FMLClientSetupEvent event) {
+
         EntityRenderers.register(ModEntities.CHAIR_ENTITY.get(), ChairRenderer::new);
 
-    }*/
-   /* @SubscribeEvent
+        MenuScreens.register(ModMenuTypes.CRATE_MENU.get(), CrateScreen::new);
+        MenuScreens.register(ModMenuTypes.FREEZER_MENU.get(), FreezerScreen::new);
+        MenuScreens.register(ModMenuTypes.SHELF_MENU.get(), ShelfScreen::new);
+        MenuScreens.register(ModMenuTypes.PEDESTAL_MENU.get(), PedestalScreen::new);
+        MenuScreens.register(ModMenuTypes.TECQUE_MENU.get(),TecqueScreen::new);
+        MenuScreens.register(ModMenuTypes.SINK_MENU.get(), SinkScreen::new);
+        MenuScreens.register(ModMenuTypes.MICROWAVE_MENU.get(), MicrowaveScreen::new);
+        MenuScreens.register(ModMenuTypes.OVEN_MENU.get(), OvenScreen::new);
+
+
+    }
+    @SubscribeEvent
     public static void registerBER(EntityRenderersEvent.RegisterRenderers event) {
         event.registerBlockEntityRenderer(ModBlockEntities.PEDESTAL_BE.get(), PedestalBlockEntityRenderer::new);
         event.registerBlockEntityRenderer(ModBlockEntities.GLASS_TECQUE.get(), TecqueBlockEntityRenderer::new);
         event.registerBlockEntityRenderer(ModBlockEntities.SHELF_BE.get(), ShelfBlockEntityRenderer::new);
         event.registerBlockEntityRenderer(ModBlockEntities.DIGITAL_CLOCK.get(), DigitalClockBlockEntityRenderer::new);
         event.registerBlockEntityRenderer(ModBlockEntities.KITCHEN_SINK.get(), KitchenSinkBlockEntityRenderer::new);
-    }*/
-   /* @SubscribeEvent
-    public static void registerScreens(MenuScreens event) {
-        event.register(ModMenuTypes.TECQUE_MENU.get(), TecqueScreen::new);
-        event.register(ModMenuTypes.PEDESTAL_MENU.get(), PedestalScreen::new);
-        event.register(ModMenuTypes.CRATE_MENU.get(), CrateScreen::new);
-        event.register(ModMenuTypes.SHELF_MENU.get(), ShelfScreen::new);
-        event.register(ModMenuTypes.OVEN_MENU.get(), OvenScreen::new);
-        event.register(ModMenuTypes.MICROWAVE_MENU.get(), MicrowaveScreen::new);
-        event.register(ModMenuTypes.SINK_MENU.get(), SinkScreen::new);
-        event.register(ModMenuTypes.FREEZER_MENU.get(), FreezerScreen::new);
-    }*/
+    }
+
 }
