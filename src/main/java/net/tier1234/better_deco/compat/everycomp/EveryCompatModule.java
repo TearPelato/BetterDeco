f5e28bdfd071f59ee1492bcaf70ac803487ded82
TearPelato <165576102+TearPelato@users.noreply.github.com>
2026-03-21 11:21:36 +0100

    Some Tests

--- src/main/java/net/tier1234/better_deco/compat/everycomp/EveryCompatModule.java
+++ src/main/java/net/tier1234/better_deco/compat/everycomp/EveryCompatModule.java
@@ -1,33 +1,52 @@
 package net.tier1234.better_deco.compat.everycomp;
 
 import net.liukrast.compat.Compat;
+import net.mehvahdjukaar.every_compat.api.EveryCompatAPI;
+import net.mehvahdjukaar.every_compat.api.PaletteStrategies;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.api.SimpleModule;
+import net.mehvahdjukaar.moonlight.api.platform.PlatHelper;
 import net.mehvahdjukaar.moonlight.api.set.wood.VanillaWoodTypes;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
+import net.minecraft.resources.ResourceLocation;
 import net.tier1234.better_deco.Constants;
 import net.tier1234.better_deco.block.custom.KitchenCounterBlock;
 import net.tier1234.better_deco.init.ModBlocks;
 
+import java.util.function.Supplier;
+
 @Compat("everycomp")
 public class EveryCompatModule extends SimpleModule {
 
-  //  public final SimpleEntrySet<WoodType, KitchenCounterBlock> kitchenCounter;
+   // public final SimpleEntrySet<WoodType, KitchenCounterBlock> kitchenCounter;
+
+    ResourceLocation tabKey = PlatHelper.getPlatform().isForge() ?
+            modRes(modId) : modRes("tab");
+
 
     public EveryCompatModule() {
         super(Constants.MOD_ID, "bd", Constants.MOD_ID);
+        EveryCompatAPI.registerModule(this);
+/*
 
-       /* kitchenCounter = SimpleEntrySet.builder(WoodType.class, "kitchen_counter",
-                        ModBlocks.OAK_KITCHEN_COUNTER.get(),()-> VanillaWoodTypes.OAK,
-                        w-> new KitchenCounterBlock(Utils.copyPropertySafe(w.planks)))
 
+
+
+
+        kitchenCounter = SimpleEntrySet.<WoodType, KitchenCounterBlock>builder(WoodType.class, "kitchen_counter",
+                (Supplier<KitchenCounterBlock>) ModBlocks.OAK_KITCHEN_COUNTER.get(), ()-> VanillaWoodTypes.OAK,
+                w-> new KitchenCounterBlock(Utils.copyPropertySafe(w.planks)))
+                .copyParentDrop()
+                .setTabKey(tabKey)
+                .defaultRecipe()
+                .addTile(getModTile("kitchen_counter"))
+                .addTexture(modRes("block/furniture/kitchen_counter/oak_kitchen_counter"), PaletteStrategies.PLANKS_STANDARD)
 
                 .build();
 
+        this.addEntry(kitchenCounter);*/
 
 
-
-        this.kitchenCounter(kitchenCounter);*/
     }
 }
