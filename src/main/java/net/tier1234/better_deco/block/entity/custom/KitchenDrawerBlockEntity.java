2ed880bb54e854e36ce329cccad35bee2d41b6ee
TearPelato <165576102+TearPelato@users.noreply.github.com>
2026-03-25 19:52:18 +0100

    Fix Kitchen Drawer wasn't saving items

--- src/main/java/net/tier1234/better_deco/block/entity/custom/KitchenDrawerBlockEntity.java
+++ src/main/java/net/tier1234/better_deco/block/entity/custom/KitchenDrawerBlockEntity.java
@@ -1,64 +1,85 @@
 package net.tier1234.better_deco.block.entity.custom;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.NonNullList;
+import net.minecraft.core.Vec3i;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.MenuProvider;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ChestMenu;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.storage.ValueInput;
-import net.minecraft.world.level.storage.ValueOutput;
-import net.neoforged.neoforge.items.ItemStackHandler;
+import net.tearpelato.deco_lib.api.block_entity.BasicLootBlockEntity;
+import net.tier1234.better_deco.block.custom.KitchenDrawerBlock;
 import net.tier1234.better_deco.block.entity.ModBlockEntities;
 
-public class KitchenDrawerBlockEntity extends RandomizableContainerBlockEntity
-        implements MenuProvider {
-    public final ItemStackHandler inventory = new ItemStackHandler(9);
-    private NonNullList<ItemStack> items = NonNullList.withSize(9, ItemStack.EMPTY);
+public class KitchenDrawerBlockEntity extends BasicLootBlockEntity
+{
+    protected KitchenDrawerBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state)
+    {
+        super(type, pos, state);
+    }
 
-    public KitchenDrawerBlockEntity(BlockPos pos, BlockState state) {
+    public KitchenDrawerBlockEntity(BlockPos pos, BlockState state)
+    {
         super(ModBlockEntities.CUSTOM_KITCHEN_DRAWER_BE.get(), pos, state);
     }
 
     @Override
-    protected Component getDefaultName() {
-        return Component.literal("Kitchen Drawer");
+    public int getContainerSize()
+    {
+        return 9;
     }
 
     @Override
-    protected AbstractContainerMenu createMenu(int id, Inventory playerInventory) {
-        return ChestMenu.oneRow(id, playerInventory);
+    protected Component getDefaultName()
+    {
+        return Component.literal("Drawer");
     }
 
     @Override
-    public int getContainerSize() {
-        return items.size();
+    protected AbstractContainerMenu createMenu(int windowId, Inventory playerInventory)
+    {
+        return new ChestMenu(MenuType.GENERIC_9x1, windowId, playerInventory, this, 1);
     }
 
     @Override
-    public NonNullList<ItemStack> getItems() {
-        return items;
+    public void onOpen(Level level, BlockPos pos, BlockState state)
+    {
+        this.playDrawerSound(state, SoundEvents.UI_LOOM_SELECT_PATTERN);
+        this.setDrawerState(state, true);
     }
 
     @Override
-    public void setItems(NonNullList<ItemStack> items) {
-        this.items = items;
+    public void onClose(Level level, BlockPos pos, BlockState state)
+    {
+        this.playDrawerSound(state, SoundEvents.UI_CARTOGRAPHY_TABLE_TAKE_RESULT);
+        this.setDrawerState(state, false);
     }
 
-    @Override
-    protected void saveAdditional(ValueOutput output) {
-        super.saveAdditional(output);
-        inventory.serialize(output);
+    private void playDrawerSound(BlockState state, SoundEvent event)
+    {
+        Vec3i directionVec = state.getValue(KitchenDrawerBlock.DIRECTION).getUnitVec3i();
+        double x = this.worldPosition.getX() + 0.5D + directionVec.getX() / 2.0D;
+        double y = this.worldPosition.getY() + 0.5D + directionVec.getY() / 2.0D;
+        double z = this.worldPosition.getZ() + 0.5D + directionVec.getZ() / 2.0D;
+        Level level = this.getLevel();
+        if(level != null)
+        {
+            level.playSound(null, x, y, z, event, SoundSource.BLOCKS, 0.5F, level.getRandom().nextFloat() * 0.1F + 0.9F);
+        }
     }
 
-    @Override
-    protected void loadAdditional(ValueInput input) {
-        super.loadAdditional(input);
-        inventory.deserialize(input);
+    private void setDrawerState(BlockState blockState, boolean open)
+    {
+        Level level = this.getLevel();
+        if(level != null)
+        {
+            level.setBlock(this.getBlockPos(), blockState.setValue(KitchenDrawerBlock.OPEN, open), 3);
+        }
     }
 }
-
