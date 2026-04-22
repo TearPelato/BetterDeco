ff591ab8af2b069c0072d3147ea7834da9460666
Tier1234 <165576102+TearPelato@users.noreply.github.com>
2026-04-22 10:38:51 +0200

    Clean up BetterDeco class by removing comments and methods
    Removed unused constructor comments and event handling methods.

--- src/main/java/net/tier1234/better_deco/BetterDeco.java
+++ src/main/java/net/tier1234/better_deco/BetterDeco.java
@@ -20,15 +20,9 @@ import org.slf4j.Logger;
 public class BetterDeco {
     public static final String MOD_ID = "better_deco";
     public static final Logger LOGGER = LogUtils.getLogger();
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
+    
     public BetterDeco(IEventBus modEventBus, ModContainer modContainer) {
-        // Register the commonSetup method for mod loading
-
-        // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
-        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
-        NeoForge.EVENT_BUS.register(this);
+    
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
@@ -46,14 +40,5 @@ public class BetterDeco {
         return ResourceLocation.fromNamespaceAndPath(MOD_ID, String.format(path, args));
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
-
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-
-    }
 
 }
