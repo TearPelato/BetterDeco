e4f15d11295cfc77d19034e723d47b134a2fcbfb
TearPelato <165576102+TearPelato@users.noreply.github.com>
2026-06-22 19:42:01 +0200

    FluidRender rework to match new internal apis
    Based on MrCrayfish code and refined by me

--- src/main/java/net/tier1234/better_deco/block/entity/renderer/custom/KitchenSinkBlockEntityRenderer.java
+++ src/main/java/net/tier1234/better_deco/block/entity/renderer/custom/KitchenSinkBlockEntityRenderer.java
@@ -1,8 +1,6 @@
 package net.tier1234.better_deco.block.entity.renderer.custom;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Axis;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.block.BlockAndTintGetter;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
@@ -14,41 +12,37 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
-import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
+import net.tearpelato.deco_lib.api.fluid.block_entity.FluidContainerBlockEntity;
 import net.tearpelato.deco_lib.api.fluid.renderer.FluidContainerRenderer;
 import net.tearpelato.deco_lib.api.fluid.renderer.core.FluidSprites;
-import net.tier1234.better_deco.block.entity.custom.KitchenSinkBlockEntity;
-import net.tier1234.better_deco.block.entity.renderer.core.render_state.KitchenSinkBlockEntityRenderState;
+import net.tearpelato.deco_lib.api.fluid.renderer.render_state.FluidRenderState;
+import net.tier1234.better_deco.block.custom.KitchenSinkBlock;
 
 import javax.annotation.Nullable;
 
-public class KitchenSinkBlockEntityRenderer implements BlockEntityRenderer<KitchenSinkBlockEntity, KitchenSinkBlockEntityRenderState> {
+public class KitchenSinkBlockEntityRenderer implements BlockEntityRenderer<FluidContainerBlockEntity, FluidRenderState> {
 
     public KitchenSinkBlockEntityRenderer(BlockEntityRendererProvider.Context context) {}
 
     @Override
-    public KitchenSinkBlockEntityRenderState createRenderState() {
-        return new KitchenSinkBlockEntityRenderState();
+    public FluidRenderState createRenderState() {
+        return new FluidRenderState();
     }
 
     @Override
-    public void extractRenderState(KitchenSinkBlockEntity blockEntity, KitchenSinkBlockEntityRenderState state,
+    public void extractRenderState(FluidContainerBlockEntity blockEntity, FluidRenderState state,
                                    float partialTick, Vec3 cameraPos,
                                    @Nullable ModelFeatureRenderer.CrumblingOverlay crumblingOverlay) {
 
         BlockEntityRenderer.super.extractRenderState(blockEntity, state, partialTick, cameraPos, crumblingOverlay);
-
         if (blockEntity.getLevel() == null) return;
-
         BlockState blockState = blockEntity.getBlockState();
-        if (!blockState.hasProperty(BlockStateProperties.HORIZONTAL_FACING)) return;
 
         state.facing = blockState.getValue(BlockStateProperties.HORIZONTAL_FACING);
         state.fluid = blockEntity.getFluid();
         state.level = blockEntity.getLevel();
         state.world = (BlockAndTintGetter) blockEntity.getLevel();
-        state.pos = blockEntity.getBlockPos();
         state.be = blockEntity;
 
         // Fluid Sprites
@@ -58,27 +52,17 @@ public class KitchenSinkBlockEntityRenderer implements BlockEntityRenderer<Kitch
         } else {
             state.fluidSprites = null;
         }
+        FluidRenderState.extract(state, blockEntity.getLevel(), blockEntity.getBlockPos());
+        if(blockState.hasProperty(BlockStateProperties.HORIZONTAL_FACING)) {
+            Direction direction = blockState.getValue(KitchenSinkBlock.DIRECTION);
+            state.box = FluidContainerRenderer.createRotatedBox(direction,2, 8, 2, 12, 16, 14);
+        }
     }
 
     @Override
-    public void submit(KitchenSinkBlockEntityRenderState state, PoseStack poseStack,
+    public void submit(FluidRenderState state, PoseStack poseStack,
                        SubmitNodeCollector submitNodeCollector, CameraRenderState camera) {
 
-        if (state.fluid == Fluids.EMPTY || state.fluidSprites == null || state.world == null) {
-            return;
-        }
-
-        poseStack.pushPose();
-        Direction dir = state.facing;
-
-        poseStack.translate(0.5, 0, 0.5);
-        poseStack.mulPose(Axis.YP.rotationDegrees(-90F * dir.get2DDataValue()));
-        poseStack.translate(-0.5, 0, -0.5);
-
-        AABB box = FluidContainerRenderer.createRotatedBox(dir, 2, 13, 2, 14, 15.9, 14);
-
-        FluidContainerRenderer.drawContainer(state, state.world, state.pos, state.be, box, poseStack, submitNodeCollector);
-
-        poseStack.popPose();
+        FluidContainerRenderer.submit(state, poseStack, submitNodeCollector);
     }
 }
